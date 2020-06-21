fea373423acd80f4ecb88faeea8578053b17c0d3
jiwonan <s2018s31@e-mirim.hs.kr>
2020-06-21 17:03:07 +0900

    DirectX 설정

--- gamePrac/gamePrac.cpp
+++ gamePrac/gamePrac.cpp
@@ -2,6 +2,8 @@
 #include "framework.h"
 #include "gamePrac.h"
 
+#include "global.h"
+
 #define MAX_LOADSTRING 100
 
 // 전역 변수:
@@ -9,9 +11,71 @@ HINSTANCE hInst;
 WCHAR szTitle[MAX_LOADSTRING];
 WCHAR szWindowClass[MAX_LOADSTRING];
 
+LPDIRECT3D9 g_pD3D;
+LPDIRECT3DDEVICE9 g_pd3dDevice;
+HWND gWindowHandle;
+
+bool GameRunning = true;
+
 ATOM                MyRegisterClass(HINSTANCE hInstance);
 BOOL                InitInstance(HINSTANCE, int);
 LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+
+void InitMyStuff()
+{
+
+}
+
+void EngineUpdate()
+{
+
+}
+
+VOID EngineRender()
+{
+    if (NULL == g_pd3dDevice)
+        return;
+
+    // Clear the backbuffer to a blue color
+    g_pd3dDevice->Clear(0, NULL, D3DCLEAR_TARGET, D3DCOLOR_XRGB(0, 0, 255), 1.0f, 0);
+
+    // Begin the scene
+    if (SUCCEEDED(g_pd3dDevice->BeginScene()))
+    {
+        // Rendering of scene objects can happen here
+
+        // End the scene
+        g_pd3dDevice->EndScene();
+    }
+
+    // Present the backbuffer contents to the display
+    g_pd3dDevice->Present(NULL, NULL, NULL, NULL);
+}
+
+HRESULT InitD3D(HWND hWnd)
+{
+    if (NULL == (g_pD3D = Direct3DCreate9(D3D_SDK_VERSION)))
+        return E_FAIL;
+
+    D3DPRESENT_PARAMETERS d3dpp;
+    ZeroMemory(&d3dpp, sizeof(d3dpp));
+    // d3dpp.Windowed = TRUE;
+    d3dpp.Windowed = false;
+    d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;
+    // d3dpp.BackBufferFormat = D3DFMT_UNKNOWN;
+    d3dpp.BackBufferWidth = WINDOW_WIDTH;
+    d3dpp.BackBufferHeight = WINDOW_HEIGHT;
+    d3dpp.BackBufferFormat = D3DFMT_A8R8G8B8;
+
+    if (FAILED(g_pD3D->CreateDevice(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, hWnd,
+        D3DCREATE_SOFTWARE_VERTEXPROCESSING,
+        &d3dpp, &g_pd3dDevice)))
+    {
+        return E_FAIL;
+    }
+
+    return S_OK;
+}
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
                      _In_opt_ HINSTANCE hPrevInstance,
@@ -33,13 +97,18 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_GAMEPRAC));
 
     MSG msg;
-
-    while (GetMessage(&msg, nullptr, 0, 0))
+    ZeroMemory(&msg, sizeof(msg));
+    while (msg.message != WM_QUIT && GameRunning == true)
     {
-        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        if (PeekMessage(&msg, NULL, 0U, 0U, PM_REMOVE))
         {
             TranslateMessage(&msg);
             DispatchMessage(&msg);
+        }
+        else
+        {
+            EngineUpdate();
+            EngineRender();
         }
     }
 
@@ -61,8 +130,8 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
     wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_GAMEPRAC));
     wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
     wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
-    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_GAMEPRAC);
-    wcex.lpszClassName  = szWindowClass;
+    wcex.lpszMenuName = nullptr;
+    wcex.lpszClassName = szWindowClass;
     wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
 
     return RegisterClassExW(&wcex);
@@ -72,16 +141,19 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
    hInst = hInstance;
 
-   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+   gWindowHandle = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
       CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
 
-   if (!hWnd)
+   if (!gWindowHandle)
    {
       return FALSE;
    }
 
-   ShowWindow(hWnd, nCmdShow);
-   UpdateWindow(hWnd);
+   InitD3D(gWindowHandle);
+   void InitMyStuff();
+
+   ShowWindow(gWindowHandle, nCmdShow);
+   UpdateWindow(gWindowHandle);
 
    return TRUE;
 }
---
+++ gamePrac/global.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <d3d9.h>
+
+#define WINDOW_WIDTH 640
+#define WINDOW_HEIGHT 480
+
+extern LPDIRECT3D9 g_pD3D;
+extern LPDIRECT3DDEVICE9 g_pd3dDevice;
+extern HWND gWindowHandle;
